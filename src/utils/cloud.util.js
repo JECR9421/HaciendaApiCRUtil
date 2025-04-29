8cfc70825f2d372dbe43d089fde78b4f7ad859cb
JECR9421 <ch2jsr@gmail.com>
2025-04-28 20:53:39 -0600

    upload with api

--- src/utils/cloud.util.js
+++ src/utils/cloud.util.js
@@ -14,16 +14,20 @@ const convertFileSystemToCloud = (path) => {
   const decode = Buffer.from(path, 'base64').toString()
   return decode.replace('C:\\Facturacion', BUCKET).replaceAll('\\', '/').slice(0, -1)
 }
+// Cambiar por axios llamado a api cloud files
+// donde bucket es path, bucket va ser quemado fedocumentsstorage y fileName = key, base64 = 'true'
 const dowloadFile = (bucket, key) => S3.getObject({
   Bucket: bucket,
   Key: key
 }).promise()
 
 const downloadFileFromCloud = async (bucket, key, localPath) => {
+  //Modificar aqui replace bucket quitar fedocumentsstorage
   const file = await dowloadFile(bucket, key)
   const dir = path.dirname(localPath)
   if (!fs.existsSync(dir)) { fs.mkdirSync(dir) }
-  fs.writeFileSync(localPath, file.Body)
+  fs.writeFileSync(localPath, file.Body) // modificar por un escritor de base64 a file
+  //fs.writeFile(filePath, Buffer.from(file.result, 'base64')
   if (!fs.existsSync(localPath)) throw new Error(`Fail downloading file ${bucket}/${key}`)
 }
 
